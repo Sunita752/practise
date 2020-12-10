b19cb3e681deb7c01a804b3ed41f45bda643b6c4
Sunita752 <74800878+Sunita752@users.noreply.github.com>
2020-12-10 16:20:20 -0600

    Add files via upload

---
+++ Practise/src/Basics1.java
@@ -0,0 +1,18 @@
+
+public class Basics1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+System.out.println("Practicing Java");		
+//Add 2 numbers 2, 3 5		
+		
+int a=2;
+int b=3;
+int Sum=a+b;
+System.out.println(Sum);
+		
+	}
+
+}
---
+++ Practise/src/day.java
@@ -0,0 +1,57 @@
+
+public class day {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("Today is Monday");
+int x =45;
+int y =9;
+int divide=x/y;
+System.out.println(divide);
+{
+int myNum = 5;               
+float myFloatNum = 5.99f;   
+char myLetter = 'D';         
+boolean myBool = true;       
+String myText = "Hello";     
+System.out.println(myNum);
+System.out.println(myFloatNum);
+System.out.println(myLetter);
+System.out.println(myBool);
+System.out.println(myText);
+ 
+if(x<y) {
+	System.out.println("false");
+}
+	else {
+	System.out.println("right");
+	
+	
+	
+	int a =3;
+	int d =3;
+	int sum=a+d;
+	System.out.println(sum);
+	
+		;
+	
+}
+ 
+	}
+	
+	
+	}
+}
+
+		 
+
+
+		  
+		
+		
+		
+		
+	
+
+
